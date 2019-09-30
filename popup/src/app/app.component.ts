32e6acd502182274d0917229d0c0daa98462d0e8
maddy13vb <45980321+maddy13vb@users.noreply.github.com>
2019-09-30 08:24:49 -0400

    popup not recognized as an element

---
+++ popup/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {Popup} from 'ng2-opd-popup';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'popup';
+  constructor(private popup: Popup) {}
+
+  ClickButton() {
+    this.popup.show();
+  }
+}
